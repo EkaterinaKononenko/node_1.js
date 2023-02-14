a6aeebf8dd183c64ffe2c2c94eb99ba5dee83cf0
EkaterinaKononenko <kaktysik4@ukr.net>
2023-02-14 14:58:19 +0200

    add readme.md

--- index.js
+++ index.js
@@ -17,25 +17,21 @@ async function invokeAction({ action, id, name, email, phone }) {
     case "list":
       const allContacts = await contacts.listContacts();
       console.table(allContacts);
-      //node index.js --action list
       break;
 
     case "get":
       const getContact = await contacts.getContactById(id);
       console.table(getContact);
-      //node index.js --action get --id 5
       break;
 
     case "add":
       const newContact = await addContact({ name, email, phone });
       console.log(newContact);
-      //node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
       break;
 
     case "remove":
       const removeContact = await contacts.removeContact(id);
       console.table(removeContact);
-      //node index.js --action remove --id=3
       break;
 
     default:
